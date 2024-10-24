31c07e19693d6908d5ec57999c6033308841b408
Luiz Otávio <luizfarias2006@hotmail.com>
2024-10-24 11:33:22 -0300

    Remove useless padding and mergin properties from layout (#3)
    * fix(ui): Remove padding and margin from layout
    
    * fix(ui): Add missing path for `portfolio` on getting image source

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{
+        margin: 0,
+        padding: 0,
+      }}>{children}</body>
     </html>
   );
 }
